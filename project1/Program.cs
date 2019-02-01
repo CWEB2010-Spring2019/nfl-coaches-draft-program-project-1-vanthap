d25d86f0ab62e282b867ab380cacc1832bf26337
vanthap <vanthap@dunwoody.edu>
2019-02-01 12:26:24 -0600

    player testing

--- project1/Program.cs
+++ project1/Program.cs
@@ -8,9 +8,37 @@ namespace project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the NLF drafting program.");
-            Console.WriteLine("This program will assist you in a simulated environment for drafting potential players.");
+            Console.WriteLine("Welcome to the NLF Drafting Program.");
+            Console.WriteLine("This program will assist you in drafting potential players in a simulated environment.");
             Console.WriteLine("Please press any key to continue");
+            Console.ReadLine();
+
+            Player aPlayer = new Player();
+            aPlayer.Name = "Dwayne Haskins";
+            aPlayer.College = "Ohio St.";
+            aPlayer.College = "$26,400,100";
+
+
+
+            
+
+
+
+
+
+        }
+
+        class Position
+        {
+            public string playerPosition { get; set; }
+
+        }
+
+        class Player
+        {
+            public string Name { get; set; }
+            public string College { get; set; }
+            public int Cost { get; set; }
         }
     }
 }
