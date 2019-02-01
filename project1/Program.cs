f838dc2cd5ecf5e7b88e1b5a4047fc974bfb5137
vanthap <vanthap@dunwoody.edu>
2019-02-01 02:43:34 -0600

    Greeting

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using static System.Console;
 
 namespace project1
 {
@@ -6,7 +8,9 @@ namespace project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the NLF drafting program.");
+            Console.WriteLine("This program will assist you in a simulated environment for drafting potential players.");
+            Console.WriteLine("Please press any key to continue");
         }
     }
 }
